9feef3af6d9f8af064a5755477b80e8538158f90
vshevtsov@kameleoon.com <vova.shevtsov@inbox.ru>
2024-03-17 15:32:19 +0300

    add json settings, init auth data from server

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -8,3 +8,6 @@ export {
 } from './model/selectors/roleSelectors';
 export type { User, UserSchema } from './model/types/user';
 export { UserRole } from './model/consts/consts';
+export { useJsonSettings } from './model/selectors/jsonSettings';
+export { saveJsonSettings } from './model/services/saveJsonSettings';
+export { initAuthData } from './model/services/initAuthData';
--- src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,10 +1,12 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import DarkIcon from '@/shared/assetes/icons/theme-dark.svg';
 import LightIcon from '@/shared/assetes/icons/theme-light.svg';
 import { Button, ButtonTheme } from '@/shared/ui/Button';
 import { Theme } from '@/shared/consts/theme';
 import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
+import { saveJsonSettings } from '@/entities/User';
+import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch';
 
 interface ThemeSwitcherProps {
     className?: string;
@@ -12,11 +14,19 @@ interface ThemeSwitcherProps {
 
 export const ThemeSwitcher = memo(({ className }: ThemeSwitcherProps) => {
     const { theme, toggleTheme } = useTheme();
+    const dispatch = useAppDispatch();
+
+    const onTogglehandler = useCallback(() => {
+        toggleTheme((newTheme) => {
+            dispatch(saveJsonSettings({ theme: newTheme }));
+        });
+    }, [dispatch, toggleTheme]);
+
     return (
         <Button
             theme={ButtonTheme.CLEAR}
             className={classNames('', {}, [className])}
-            onClick={toggleTheme}
+            onClick={onTogglehandler}
         >
             {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
         </Button>
